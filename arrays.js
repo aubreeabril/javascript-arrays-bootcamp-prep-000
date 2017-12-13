e7c92b0c9f908f02a287e6fa52f7f3b2dbc4806e
Aubree Abril <aubree.abril@gmail.com>
2017-12-13 02:39:10 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,10 +1,11 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
 function addElementToBeginningOfArray(array, element) {
-  var newArray = return [...array, element]
+  var newArray = [...array, element]
+  return newArray
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  return array.unshift(element);
+  return array.unshift(element)
 
 }
